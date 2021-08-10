d3bcb2d2781cf2fea6b999e78ffc22108b7ed297
aakashsadnani <A00235504@mycambrian.ca>
2021-08-10 18:34:15 -0400

    Added course description page and course title and description model and passed all data to view page

---
+++ MyCourses/Courses.swift
@@ -0,0 +1,18 @@
+//
+//  Courses.swift
+//  MyCourses
+//
+//  Created by aakash sadnani on 10/08/21.
+//
+
+import Foundation
+
+class Courses{
+    var name: String
+    var desc: String
+    
+    init(name: String, desc: String){
+        self.name = name
+        self.desc = desc
+    }
+}
--- MyCourses/ViewDetailsViewController.swift
+++ MyCourses/ViewDetailsViewController.swift
@@ -10,17 +10,31 @@ import UIKit
 
 class ViewDetailsViewController: UIViewController {
     
-//    @IBOutlet weak var lbl: UILabel!
     
     
+    var mycourses: Courses!
     var image = UIImage()
     var name = ""
+    var course: String!
+    var titles: String!
+    @IBOutlet weak var coursedescription: UILabel!
+    
+    @IBOutlet weak var coursedescriptionfulltext: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("second page")
+       
+        
+        coursedescription.text = titles.description
+        
+        coursedescriptionfulltext.text = course.description
+        
+        
+        
     }
+    
     
 //    override func viewDidLoad() {
 //          super.viewDidLoad()
